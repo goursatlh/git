c4e50658f699d99d76b5cce88d0d576ea482275a
walter <walter@walterdeMac.local>
2015-07-01 03:00:28 -0700

    second version: study python: class

--- test.py
+++ test.py
@@ -2,3 +2,67 @@
 print("hello world.","fuck you then")
 name = input("please input your name: ")
 print("hello", name)
+
+#enumerate()
+import string
+s = string.ascii_lowercase
+e = enumerate(s)
+print(s)
+print(list(e))
+
+for i, j in enumerate(s):
+    print(i,j)
+
+#class
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def print_score(self):
+        print(self.name, self.score)
+
+boy = Student("jerry", 20)
+boy.print_score()
+
+#list 
+classmates = ["jerry", "tom", "carl"]
+print(classmates)
+print(len(classmates))
+print(classmates[0])
+print(classmates[-1])
+classmates.append("walter")
+print(classmates)
+classmates.insert(1, "james")
+print(classmates)
+classmates.pop()
+print(classmates)
+classmates.pop(1)
+print(classmates)
+classmates[0] = 1100
+print(classmates)
+
+for stu in classmates:
+    print(stu)
+
+#for
+sum = 0
+for i in [0,1,2,3,4,5,6]:
+    sum += i
+print(sum)
+
+b = range(100)
+print(b)
+b = list(range(100))
+print(b)
+
+#how to confirm an obj is an iterable obj
+from collections import Iterable
+i = isinstance("abc", Iterable)
+print(i)
+i = isinstance(123, Iterable)
+print(i)
+i = isinstance([1, 2, 3], Iterable)
+print(i)
+
+for i,value in enumerate([1, 2, 3]):
+    print(i, value)
