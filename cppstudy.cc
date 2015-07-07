535fe5a4239ef425553b8f70c7d3f02bd8cd0800
walter <walter@walterdeMac.local>
2015-07-07 18:07:50 +0800

    c++ study: copy initialize

--- cppstudy.cc
+++ cppstudy.cc
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 
-using namespace std;
+//using namespace std; // don't use this style code, that may cause name conflict
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
+#if 0 
 class A
 {
     public:
@@ -21,11 +26,16 @@ class B: public A
         }
 };
 
-#if 1 
 int main()
 {
     A a;
     B b;
+    char cc[] = "hello world\n";
+    string s(10, 'f');
+
+    cout<<cc;
+    cout<<s<<endl;
+
     a.print();
     b.print();
 
@@ -59,3 +69,98 @@ int main()
     system("pause"); 
 } 
 #endif
+
+#if 0
+//direct initialize and copy initialize
+class ClassTest
+{
+    public:
+        ClassTest()
+        {
+            c[0] = '\0';
+            cout<<"ClassTest()"<<endl;
+        }
+
+        ClassTest& operator=(const ClassTest &ct)
+        {
+            strcpy(c, ct.c);
+            cout<<"ClassTest& operator=(const ClassTest &ct)"<<endl;
+            return *this;
+        }
+        
+        ClassTest(const char *pc)
+        {
+            strcpy(c, pc);
+            cout<<"ClassTest (const char *pc)"<<endl;
+        }
+    //private:
+        ClassTest(const ClassTest& ct)
+        {
+            strcpy(c, ct.c);
+            cout<<"ClassTest(const ClassTest& ct)"<<endl;
+        }
+
+    private:
+        char c[256];
+};
+
+int main()
+{
+    cout<<"ct1: ";
+    ClassTest ct1("ab");
+    cout<<"ct2: ";
+    ClassTest ct2 = "ab";
+    cout<<"ct3: ";
+    ClassTest ct3 = ct1;
+    cout<<"ct4: ";
+    ClassTest ct4(ct1);
+    cout<<"ct5: ";
+    ClassTest ct5 = ClassTest();
+    return 0;
+}
+#endif
+
+class CExample 
+{
+    private:
+        int a;
+
+    public:
+
+        CExample(int b)
+        { 
+            a = b;
+            cout<<"creat: "<<a<<endl;
+        }
+
+
+        CExample(const CExample& C)
+        {
+            a = C.a;
+            cout<<"copy"<<endl;
+        }
+
+        ~CExample()
+        {
+            cout<< "delete: "<<a<<endl;
+        }
+
+        void Show ()
+        {
+            cout<<a<<endl;
+        }
+};
+
+void g_Fun(CExample C)
+{
+    cout<<"test"<<endl;
+}
+
+int main()
+{
+    CExample test(1);
+
+    g_Fun(test);
+
+    return 0;
+}
