67644f8670a5aa4b59b403e57604d7fbe9baab26
goursatlh <goursatlh@163.com>
2015-08-31 23:43:09 +0800

    c++ study: set/multiset

--- cppstudy.cc
+++ cppstudy.cc
@@ -13,22 +13,25 @@ using std::string;
 using std::vector;
 //using std::initializer_list;
 #endif
-
-
 #if 1
 #include <iostream>
+#include <vector>
 #include <string>
 #include <map>
 #include <set>
+
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::map;
 using std::set;
+using std::multiset;
+using std::vector;
 
 int main()
 {
+#if 0
    map<string, int> count;
    //map<string, int> count1 = {{"hello", 2}};
    //set<string> exclude = {"fuck"};
@@ -46,6 +49,20 @@ int main()
    {
        cout<<w.first<<" occurs "<<w.second<<((w.second > 1) ? "times" : "time")<<endl;
    }
+#endif
+
+   vector<int> ivec;
+   for (vector<int>::size_type i = 0; i < 10; i++)
+   {
+       ivec.push_back(i);    
+       ivec.push_back(i);    
+   }
+   set<int> iset(ivec.cbegin(), ivec.cend());
+   multiset<int> miset(ivec.cbegin(), ivec.cend());
+
+   cout<<ivec.size()<<endl;
+   cout<<iset.size()<<endl;
+   cout<<miset.size()<<endl;
    return 0;
 }
 #endif
