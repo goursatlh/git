6cab0aad86981cbd2fa919eca64e2bcd3e379dde
walter <walter@walterdeMac.local>
2015-10-28 17:58:10 +0800

    c++ study: weak_ptr

--- cppstudy.cc
+++ cppstudy.cc
@@ -705,7 +705,91 @@ int main()
 }
 #endif
 
-#if 1 //smart pointer
+#if 1 // smart pointer 2
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Controller 
+{
+    public:
+        int Num;
+        wstring Status;
+        vector<weak_ptr<Controller>> others;
+        explicit Controller(int i) : Num(i) , Status(L"On")
+        {
+            wcout << L"Creating Controller" << Num << endl;
+        }
+
+        ~Controller()
+        {
+            wcout << L"Destroying Controller" << Num << endl;
+        }
+
+
+        void CheckStatuses() const
+        {
+            for_each(others.begin(), others.end(), [] (weak_ptr<Controller> wp)
+            {
+                try
+                {
+                    auto p = wp.lock();
+                    wcout << L"Status of " << p->Num << " = " << p->Status << endl;
+                }
+
+                catch (bad_weak_ptr b)
+                {
+                    wcout << L"Null object" << endl;
+                }                
+            });
+        }
+};
+
+void RunTest()
+{
+    vector<shared_ptr<Controller>> v;
+
+    v.push_back(shared_ptr<Controller>(new Controller(0)));
+    v.push_back(shared_ptr<Controller>(new Controller(1)));
+    v.push_back(shared_ptr<Controller>(new Controller(2)));
+    v.push_back(shared_ptr<Controller>(new Controller(3)));
+    v.push_back(shared_ptr<Controller>(new Controller(4)));
+
+
+    for (int i = 0 ; i < v.size(); ++i)
+    {
+        for_each(v.begin(), v.end(), [v,i] (shared_ptr<Controller> p)
+        {
+            if(p->Num != i)
+            {
+                v[i]->others.push_back(weak_ptr<Controller>(p));
+                wcout << L"push_back to v[" << i << "]: " << p->Num << endl;
+            }
+        });        
+    }
+
+    for_each(v.begin(), v.end(), [](shared_ptr<Controller>& p)
+    {
+        wcout << L"use_count = " << p.use_count() << endl;
+        p->CheckStatuses();
+    });
+}
+
+int main()
+{    
+    RunTest(); 
+    wcout << L"Press any key" << endl;
+    char ch;
+    cin.getline(&ch, 1);
+}
+
+#endif
+
+#if 0 //smart pointer
 #include <iostream>
 #include <string>
 #include <memory>
@@ -1679,18 +1763,16 @@ int main()
 }
 #endif
 
-#if 0 // iterator
+#if 0 // for_each api
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
 int main()
 {
     string c("hello world");
-    iterator j;
-    if (c.begin() != c.end())
-    {
-
-        auto i = c.begin();
-        cout<<i<<endl;
-        i++;
-    }
+    for_each(c.begin(), c.end(), [] (char &s) { cout<<s<<endl; });
 
     return 0;
 }
