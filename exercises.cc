44a7bb38f8bb23b83be6e92a4a14b8fe9a26c66f
goursatlh <goursatlh@163.com>
2015-11-23 01:25:56 -0800

    fs open/link

--- exercises.cc
+++ exercises.cc
@@ -1,4 +1,49 @@
-#if 1 // 3.13
+#if 0
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+int main()
+{
+    struct stat buf;
+    int fd = 0;
+    stat("test.txt",&buf);
+    printf("1.link=%d\n",buf.st_nlink);//未打开文件之前测试链接数
+
+    fd=open("test.txt",O_RDONLY);//打开已存在文件test.txt
+    stat("test.txt",&buf);
+    printf("2.link=%d\n",buf.st_nlink);//测试链接数
+
+    close(fd);//关闭文件test.txt
+    stat("test.txt",&buf);
+    printf("3.link=%d\n",buf.st_nlink);//测试链接数
+
+    link("test.txt","test2.txt");//创建硬链接test2.txt
+    stat("test.txt",&buf);
+    printf("4.link=%d\n",buf.st_nlink);//测试链接数
+
+    unlink("test2.txt");//删除test2.txt
+    stat("test.txt",&buf);
+    printf("5.link=%d\n",buf.st_nlink);//测试链接数
+
+    fd=open("test.txt",O_RDONLY);//打开已存在文件test.txt
+    stat("test.txt",&buf);
+    printf("6.link=%d\n",buf.st_nlink);//测试链接数
+
+    unlink("test.txt");//删除test.txt
+    fstat(fd,&buf);
+    printf("7.link=%d\n",buf.st_nlink);//测试链接数
+
+    close(fd);
+    fstat(fd,&buf);
+    printf("8.link=%d\n",buf.st_nlink);//测试链接数
+    
+    return 0;
+}
+#endif
+
+#if 0 // 3.13
 #include <iostream>
 #include <vector>
 
