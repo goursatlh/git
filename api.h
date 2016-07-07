d1edcfdaff9c2688cc16d58e019f996b8e591277
goursatlh <goursatlh@163.com>
2016-07-07 02:30:45 -0700

    algo: shell2

--- api.h
+++ api.h
@@ -47,11 +47,54 @@ void sort_bubble(vector<T> &vec, int left, int right, const int find)
     }
     gettimeofday(&tvend, NULL);
     timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
-    cout<<"bubble sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us"<<endl;
+    cout<<"sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us by bubble sort"<<endl;
 }
 
 template <typename T>
-void sort_insert(vector<T> &vec, int left, int right, const int index)
+void __sort_insert(vector<T> &vec, int left, int right)
+{
+    for (int i = left+1; i <= right; i++)
+    {
+        for (int j = i; j > left && (less(vec[j], vec[j-1]) > 0); j--)
+        {
+            exchange(vec[j], vec[j-1]);
+        }
+    }
+}
+
+//#define DEBUG
+template <typename T>
+void __sort_shell(vector<T> &vec, int left, int right)
+{
+    int h = 1;
+    while (h < (right-left+1)/3)
+        h = 3*h+1;
+#ifdef DEBUG
+    for (auto iter = vec.begin(); iter != vec.end(); iter++)
+        cout<<*iter<<" ";
+    cout<<endl;
+#endif
+    while (h >= 1)
+    {
+        for (int i = left+h; i <= right; i++)
+        {
+            for (int j = i; j >=h && (less(vec[j], vec[j-h]) > 0); j -= h)
+            {
+                exchange(vec[j], vec[j-h]);
+            }
+        }
+#ifdef DEBUG
+        cout<<"h "<<h<<endl;
+        for (auto iter = vec.begin(); iter != vec.end(); iter++)
+            cout<<*iter<<" ";
+        cout<<endl;
+#endif
+        h = h/3;
+    }
+}
+
+template <typename T>
+void sort_insert_ex(vector<T> &vec, int left, int right, const int index)
 {
     struct timeval tvstart, tvend;
     long timespend = 0;
@@ -61,7 +104,7 @@ void sort_insert(vector<T> &vec, int left, int right, const int index)
     {
         vectmp.push_back(vec[i]);
     }
-    sort_bubble(vectmp, 0, index-1, 0);
+    __sort_insert(vectmp, 0, index-1);
 
     for (int i = index; i <= right; i++)
     {
@@ -98,7 +141,7 @@ void sort_insert(vector<T> &vec, int left, int right, const int index)
     }
     gettimeofday(&tvend, NULL);
     timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
-    cout<<"insert sort result is "<<vectmp[index-1]<<" time spend: "<<timespend<<" us"<<endl;
+    cout<<"sort result is "<<vectmp[index-1]<<" time spend: "<<timespend<<" us by insert_ex sort"<<endl;
 }
 
 // 0 1 2 3 4 5
@@ -149,6 +192,64 @@ void sort_quick(vector<T> &vec, int left, int right, int find)
     __sort_quick(vec, left, right);
     gettimeofday(&tvend, NULL);
     timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
-    cout<<"quick sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us"<<endl;
+    cout<<"sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us by quick sort"<<endl;
 
+}
+
+template <typename T>
+void sort_insert(vector<T> &vec, int left, int right, int find)
+{
+    struct timeval tvstart, tvend;
+    long timespend = 0;
+    gettimeofday(&tvstart, NULL);
+    __sort_insert(vec, left, right);
+    gettimeofday(&tvend, NULL);
+    timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
+    cout<<"sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us by insert sort"<<endl;
+
+}
+
+template <typename T>
+void sort_shell(vector<T> &vec, int left, int right, int find)
+{
+    struct timeval tvstart, tvend;
+    long timespend = 0;
+    gettimeofday(&tvstart, NULL);
+    __sort_shell(vec, left, right);
+    gettimeofday(&tvend, NULL);
+    timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
+    cout<<"sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us by shell sort"<<endl;
+
+}
+
+template <typename T>
+void __sort_choose(vector<T> &vec, int left, int right)
+{
+    for (int i = left; i <= right; i++)
+    {
+        int max = vec[i];
+        int index = i;
+        for (int j = i+1; j <= right; j++)
+        {
+            if (vec[j] > max)
+            {
+                max = vec[j];
+                index = j;
+            }
+        }
+        if (i != index)
+            exchange(vec[i], vec[index]);
+    }
+}
+
+template <typename T>
+void sort_choose(vector<T> &vec, int left, int right, int find)
+{
+    struct timeval tvstart, tvend;
+    long timespend = 0;
+    gettimeofday(&tvstart, NULL);
+    __sort_choose(vec, left, right);
+    gettimeofday(&tvend, NULL);
+    timespend = (tvend.tv_sec-tvstart.tv_sec)*1000000+(tvend.tv_usec-tvstart.tv_usec);
+    cout<<"sort result is "<<vec[find-1]<<" time spend: "<<timespend<<" us by choose sort"<<endl;
 }
