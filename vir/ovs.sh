7a88b5941bb256eaca58e9638d387dd810f8245a
goursatlh <goursatlh@163.com>
2017-05-12 02:02:18 -0700

    vir study 2

--- vir/ovs.sh
+++ vir/ovs.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 BRIDGE="ovs-br0"
-echo ${BRIDGE}
-ovs-vsctl add-br ${BRIDGE}
-ifconfig ${BRIDGE} 10.0.3.1/24 up
-echo $*
 if [ $1 -eq 1 ];then
     echo "start my-container"
+    ovs-vsctl add-br ${BRIDGE}
+    ifconfig ${BRIDGE} 10.0.3.1/24 up
     lxc-start -n my-container -d --logfile /var/lxclog
 else
     echo "stop my-container"
