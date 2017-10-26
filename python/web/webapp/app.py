5a832d4dd1638dd1f733f39764798ee837e6313e
goursatlh <goursatlh@163.com>
2017-10-26 00:20:28 -0700

    python:QYWeb-0.1.0

---
+++ python/web/webapp/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.5
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time
+from aiohttp import web
+from datetime import datetime
+
+def index(request):
+    return web.Response(body=b'<h1>Hello world</h1>', content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '10.88.39.145', 9000)
+    logging.info('Server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
