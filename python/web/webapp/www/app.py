9deb39a17bf8f12ffa3f6175471afc9fd2efc8ab
goursatlh <goursatlh@163.com>
2017-12-25 01:31:51 -0800

    python: web-app ver1.0

--- python/web/webapp/www/app.py
+++ python/web/webapp/www/app.py
@@ -3,14 +3,18 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from aiohttp import web
 from datetime import datetime
+from coroweb import add_routes
 
+'''
 def index(request):
     return web.Response(body=b'<h1>Hello world</h1>', content_type='text/html')
+'''
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
+    #app.router.add_route('GET', '/', index)
+    add_routes(app, 'handlers')
     srv = yield from loop.create_server(app.make_handler(), '10.88.39.145', 9000)
     logging.info('Server started at http://10.88.39.145:9000...')
     return srv
---
+++ python/web/webapp/www/coroweb.py
@@ -0,0 +1,71 @@
+
+#!/usr/bin/env python3.5
+
+import asyncio, os, functools, inspect
+from aiohttp import web
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+class RequestHandler(object):
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+
+    @asyncio.coroutine
+    def __call__(self, request):
+        kw = {}
+        kw['request'] = request
+        r = yield from self._func(**kw)
+        return r
+
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    # -- app.router.add_route('GET', '/', index) --
+    if path is None or method is None:
+        raise ValueError('@get not define in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            print('xxxxxxxxxxxxxxxxxxxx  mod is ', method, path)
+            if method and path:
+                add_route(app, fn)
+
+
+
+
+
+
+
+
+
+
+
+
+
