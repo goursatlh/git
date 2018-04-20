db9ce1f04761a6a9644baa7c794a70e98ae7c896
goursatlh <goursatlh@163.com>
2018-04-20 01:54:55 -0700

    c: sem_wait/sem_post

--- IPC/sharedmem/share4.c
+++ IPC/sharedmem/share4.c
@@ -8,15 +8,27 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
+#include <sys/file.h>
+/* shared memoroy and mutex
+ * mmap
+ * sem_wait/sem_post
+ * flock
+ *
+ * */
+
 
 /* You must use 'gcc -o share4 share4.c -lrt' to compile the program to link the realtime lib */
+
+
+#define SEM_NAME "/mysem"
+
 void error_and_die(const char *msg)
 {
 	perror(msg);
 	exit(EXIT_FAILURE);
 }
 
-void *mmap_func()
+void *mmap_func(int *fd_out)
 {
     int r;
 	const char *memname = "sample";
@@ -32,14 +44,16 @@ void *mmap_func()
     void *ptr = mmap(NULL, region_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (ptr == MAP_FAILED)
 		error_and_die("mmap");
-	close(fd);
+	//close(fd);
+    *fd_out = fd;
     return ptr;
 }
 
 int main(int argc, char *argv[])
 {
 	int r;
-#if 0
+    int val = 0;
+    int *d;
 	const char *memname = "sample";
 	const size_t region_size = sysconf(_SC_PAGE_SIZE);
     sem_t *sem_p = NULL; 
@@ -52,27 +66,36 @@ int main(int argc, char *argv[])
 	if (r != 0)
 		error_and_die("ftruncate");
 
-    //sem_p = sem_open("mysem", O_CREAT, 0666, 0);
 	void *ptr = mmap(NULL, region_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (ptr == MAP_FAILED)
 		error_and_die("mmap");
 	close(fd);
+    d = (int *)ptr;
+    
+    sem_unlink(SEM_NAME);
+    if (!sem_p)
+    {
+        sem_p = sem_open(SEM_NAME, O_CREAT, 0666, 1);
+        if (sem_p == SEM_FAILED)
+            error_and_die("fail to open the sem.\n");
+        r = sem_getvalue(sem_p, &val);
+        printf("sem init vaule %d, ret %d\n", val, r);
+#if 0
+        sem_post(sem_p);
+        r = sem_getvalue(sem_p, &val);
+        printf("sem set vaule to %d, ret %d\n", val, r);
 #endif
+    }
 
 	pid_t pid = fork();
 	if (pid == 0)
 	{
         int i = 100;
-        sem_t *sem_p = NULL; 
-        if (!sem_p)
-        {
-            sem_p = sem_open("mysem", O_CREAT, 0666, 0);
-            if (sem_p == SEM_FAILED)
-                error_and_die("fail to open the sem.\n");
-        }
-        
+        r = sem_getvalue(sem_p, &val);
+        printf("son sem vaule %d, ret %d\n", val, r);
         sem_wait(sem_p);
-		int *d = (int *)mmap_func();
+		//int *d = (int *)mmap_func(&fd);
+        //flock(fd, LOCK_EX);
         while (i-- > 0)
         {
             printf("son: %d\n", *d);
@@ -86,32 +109,30 @@ int main(int argc, char *argv[])
 	{
 		int status;
         int i = 100;
-        sem_t *sem_p = NULL; 
-        
-        if (!sem_p)
-        {
-             sem_p = sem_open("mysem", 0);
-             if (sem_p == SEM_FAILED)
-                error_and_die("fail to open the sem.\n");
-        }
+        r = sem_getvalue(sem_p, &val);
+        printf("parent sem vaule %d, ret %d\n", val, r);
         sem_wait(sem_p);
-		int *d1 = (int *)mmap_func();
+		//int *d1 = (int *)mmap_func(&fd);
+        //flock(fd, LOCK_EX);
         while (i-- > 0)
         {
-            printf("parent: %d\n", *d1);
+            printf("parent: %d\n", *d);
         }
         sem_post(sem_p);
 		waitpid(pid, &status, 0);
 		//printf("child wrote %#lx\n", *(u_long *) ptr);
 	}
 
-	//r = munmap(ptr, region_size);
+	r = munmap(ptr, region_size);
 	if (r != 0)
 		error_and_die("munmap");
-#if 0
-	r = shm_unlink(memname);
+	
+    r = shm_unlink(memname);
 	if (r != 0)
 		error_and_die("shm_unlink");
-#endif
+    
+    r = sem_getvalue(sem_p, &val);
+    printf("out sem vaule %d, ret %d\n", val, r);
+    sem_unlink(SEM_NAME);
 	return 0;
 }
