b6c8c7fc54d0abb97fc044a786dc0f63971cf3d5
goursatlh <goursatlh@163.com>
2018-10-09 01:27:11 -0700

    vim: add vim-init

--- tools/cs.sh
+++ tools/cs.sh
@@ -1,7 +1,12 @@
 rm -f cscope*
-rm -f tags
+rm -f tags                    
+rm -f GPATH GRTAGS GTAGS
 
-find . -name "*.c" -or -name "*.mib" -or -name "*.js" -or -name "*.php" -or -name "*.h" -or -name "*Makefile*" >cscope.files
-cscope -Rbqk &
-ctags -R &
+#find . -follow -name "*.c" -or -name "*.mib" -or -name "*.js" -or -name "*.php" -or -name "*.h" -or -name "*Makefile*" >cscope.files
+#cscope -Rbqk &
+#ctags -R &
 
+# use gtags instead of ctags/cscope to do the code index/reference
+find . -follow -name "*.c" -or -name "*.mib" -or -name "*.js" -or -name "*.php" -or -name "*.h" -or -name "*Makefile*" >gtags.files
+gtags 
+~        
---
+++ tools/vim-init.sh
@@ -0,0 +1,12 @@
+
+
+
+
+git clone xxx.gitt
+
+cp xx  ~/.vimrc
+cp -r xx ~/.vim
+cp cs.sh ~/
+
+
+
