6448c51a5054d1a85214aefb6141e83c858d033b
goursatlh <goursatlh@163.com>
2019-03-21 00:53:35 -0700

    python: data analysis start

---
+++ python/data-analysis.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from pandas import Series
+from pandas import DataFrame
+
+format_begin = "\033[1;32;43m"
+format_begin_2 = "\033[1;35m"
+format_begin_3 = "\033[5;32m"
+format_end  = "\033[0m"
+def printx(para):
+    print(format_begin, para, format_end, sep='') # sepatator: sep="", end=""
+def printy(para):
+    print(format_begin_2, para, format_end, sep='') # sepatator: sep="", end=""
+def print_flash(para):
+    print(format_begin_3, para, format_end, sep='') # sepatator: sep="", end=""
+# Series
+obj = Series([1,2,"walter"])
+print(obj)
+print("index: ", obj.index)
+print("values: ", obj.values)
+
+obj2 = Series([99,2,-1], index = ['b', 'a', 'd'])
+print(obj2)
+
+printx("DataFrame:                                                                                   ")
+# DataFrame
+data = {'state': ["Ohio", "Ohio", "Nevada"],
+        'year': [2001, 1998, 2019],
+        'pop': [1.5, 0.9, 2.2]}
+print(type(data))
+print(data)
+
+df = DataFrame(data)
+print(type(data))
+print(df)
+print("\033[5;32mcolumns: \033[0m!", df.columns)
+print(df["state"])
+print(df.state)
+
+printy("row: ")
+print(df.loc[0])
+
+print("add a new column for all index: ")
+df["debt"] = 12.0
+print(df)
+
+printy("add a new column for specific index: ")
+price = Series([9000], index = [1])
+df["price"] = price
+print(df)
+
+printy("add a new column by math")
+df["eastern"] = df["state"] == "Ohio"
+print(df)
+
+printy("del a column")
+del df["eastern"]
+print(df)
+
+
+
