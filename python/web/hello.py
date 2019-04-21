f82bad9a1d84d024b894b0e70092e14299376d4d
goursatlh <goursatlh@163.com>
2019-04-20 23:19:45 -0700

    sync code

--- python/web/hello.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-def application(environ, start_response):
-    print(environ)
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, Web</h1>'] 
