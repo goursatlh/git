0dbabeed11677995f066c454db6c2851dc99bb09
goursatlh <goursatlh@163.com>
2019-07-22 22:42:29 -0700

    gettid

--- test.c
+++ test.c
@@ -1,6 +1,205 @@
 
 
-#if 1 // redirect stdout to file
+#if 1 // get pid by process name
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+ 
+#define BUF_SIZE 1024
+
+void getPidByName(pid_t *pid, char *task_name)
+{
+    DIR *dir;
+    struct dirent *ptr;
+    FILE *fp;
+    char filepath[50];
+    char cur_task_name[50];
+    char buf[BUF_SIZE];
+
+    dir = opendir("/proc"); 
+    if (NULL != dir)
+    {
+        while ((ptr = readdir(dir)) != NULL) //循环读取/proc下的每一个文件/文件夹
+        {
+            //如果读取到的是"."或者".."则跳过，读取到的不是文件夹名字也跳过
+            if ((strcmp(ptr->d_name, ".") == 0) || (strcmp(ptr->d_name, "..") == 0))
+                continue;
+            if (DT_DIR != ptr->d_type)
+                continue;
+           
+            sprintf(filepath, "/proc/%s/status", ptr->d_name);//生成要读取的文件的路径
+            fp = fopen(filepath, "r");
+            if (NULL != fp)
+            {
+                if( fgets(buf, BUF_SIZE-1, fp)== NULL ){
+                    fclose(fp);
+                    continue;
+                }
+                sscanf(buf, "%*s %s", cur_task_name);
+        
+                //如果文件内容满足要求则打印路径的名字（即进程的PID）
+                if (!strcmp(task_name, cur_task_name)){
+                    sscanf(ptr->d_name, "%d", pid);
+                }
+                fclose(fp);
+            }
+        }
+        closedir(dir);
+    }
+}
+
+void getNameByPid(pid_t pid, char *task_name) {
+    char proc_pid_path[BUF_SIZE];
+    char buf[BUF_SIZE];
+
+    sprintf(proc_pid_path, "/proc/%d/status", pid);
+    FILE* fp = fopen(proc_pid_path, "r");
+    if(NULL != fp){
+        if( fgets(buf, BUF_SIZE-1, fp)== NULL ){
+            fclose(fp);
+        }
+        fclose(fp);
+        sscanf(buf, "%*s %s", task_name);
+    }
+}
+
+void main(int argc, char** argv)
+{
+    char task_name[50];
+    pid_t pid = getpid();
+
+    printf("pid of this process:%d\n", pid);
+    getNameByPid(pid, task_name);
+    
+    /*
+    strcpy(task_name, argv[0]+2);
+    printf("task name is %s\n", task_name);
+    getPidByName(task_name);
+    */
+    printf("getNameByPid:%s\n", task_name);
+    getPidByName(&pid, task_name);
+    printf("getPidByName:%d\n", pid);
+    sleep(15);
+}
+#endif
+
+#if 0
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+typedef uint32_t UINT32;
+void fun4(void)
+{
+  void* array[10] = {0};
+  UINT32 size = 0;
+  char **strframe = NULL;
+  UINT32 i = 0, j = 0;
+
+  size = backtrace(array, 10);
+  strframe = (char **)backtrace_symbols(array, size);
+
+  printf("print call frame now:\n");
+  for(i = 0; i < size; i++){
+    printf("frame %d -- %s\n", i, strframe[i]);
+  }
+
+  if(strframe)
+  {
+    free(strframe);
+    strframe = NULL;
+  }
+}
+
+void fun3(void)
+{
+  fun4();
+}
+
+void fun2(void)
+{
+  fun3();
+}
+
+void fun1(void)
+{
+  fun2();
+}
+
+int main(void)
+{
+  fun1();
+  return 0;
+}
+
+#endif
+
+
+
+
+
+
+
+
+#if 0 // thread lock
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+ 
+int main(void)
+{
+	int err;
+	struct timespec tout;
+	struct tm *tmp;
+	char buf[64];
+	
+	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+	
+	//1
+	pthread_mutex_lock(&lock);
+	printf("mutex is locked.\n");
+	clock_gettime(CLOCK_REALTIME, &tout);
+	tmp = localtime(&tout.tv_sec);
+	strftime(buf, sizeof(buf), "%r", tmp);   //strftime(char *str, size_t maxsize, const char *fmt, struct tm *time)
+											 //按照参数fmt所设定格式将time类型的参数格式化为日期时间信息，然后存储在字符串str中(至多maxsize 个字符)
+											 //参考http://ganquan.info/standard-c/function/strftime
+	printf("Current time is %s.\n", buf);
+	tout.tv_sec += 10;   //延迟10s
+	
+	//2
+	err = pthread_mutex_timedlock(&lock, &tout);
+	clock_gettime(CLOCK_REALTIME, &tout);
+	tmp = localtime(&tout.tv_sec);
+	strftime(buf, sizeof(buf), "%r", tmp);
+	printf("The time is now %s\n", buf);
+	
+	if(err == 0)
+	{
+		printf("mutex locked again!\n");
+	}
+	else
+	{
+		printf("Can't lock mutex again: %s\n", strerror(err));
+	}
+	
+	return 0;
+}
+
+#endif
+#if 0
+#include <stdio.h>
+int main()
+{
+    char cmd[256] = {0};
+    int a = sprintf(cmd, "test ");
+    printf("%d\n", a);
+    sprintf(cmd+a, "fuck \n");
+    printf("%s", cmd);
+    return 0;
+}
+#endif
+#if 0 // redirect stdout to file
 #include <stdio.h>
 int main()
 {
@@ -674,6 +873,7 @@ int main()
     return 0;
 }
 #endif
+
 #if 0
 #include <stdio.h>
 #include <unistd.h>
@@ -874,7 +1074,7 @@ int main()
 }
 #endif
 
-#if 0				//flock
+#if 0			//flock
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/file.h>
@@ -883,10 +1083,13 @@ int main()
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/syscall.h>
 
+#define gettid() syscall(SYS_gettid)
 
 void readfile_thread(char *filepath)
 {
+    printf("thread process : %d %d\n", getpid(), gettid());
 	int fd = open(filepath, O_RDONLY);
 
 	if (fd == -1)
@@ -907,6 +1110,7 @@ int main()
 	char filepath[] = "/home/wt/code/git/tmp.txt";
 	pthread_t thread_id;
 
+    printf("main process : %d %d\n", getpid(), gettid());
 	pthread_create(&thread_id, NULL, readfile_thread, filepath);
 
 	sleep(2);
