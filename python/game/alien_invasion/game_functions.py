cf8ed8ca84be0ad1490f03e9aaba47c7ef25359d
goursatlh <goursatlh@163.com>
2020-02-09 16:58:59 +0800

    python: ai versio 0.1

--- python/game/alien_invasion/game_functions.py
+++ python/game/alien_invasion/game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 SHIFT_OFFSET = 10
 def check_events(ai_settings, screen, ship, bullets):
@@ -24,16 +25,52 @@ def check_events(ai_settings, screen, ship, bullets):
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, bullets, aliens):
     screen.fill(ai_settings.bg_color)
     ship.update()
     ship.blitme()
-    alien.blitme()
+    #alien.blitme()
+    aliens.draw(screen) # display all the elements of group in the screen
+
     for bullet in bullets.sprites():
         bullet.update()
         bullet.draw_bullet()
     pygame.display.flip()
 
 def update_bullets(aliens, bullets):
+    bullets.update()
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    #collisions = pygame.sprite.spritecollide(alien, bullets, True)
+
+
+def create_fleet(ai_settings, screen, aliens):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_alien_x = int(available_space_x/(2 * alien_width))
+    for alien_num in range(number_alien_x):
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_num
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
+def change_fleet_direction(ai_settings, aliens):
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def check_fleet_edges(ai_settings, aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+
+
+
+
 
--- python/game/alien_invasion/settings.py
+++ python/game/alien_invasion/settings.py
@@ -1,10 +1,16 @@
 class Settings():
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230,230,230)
         self.bullet_speed_factor = 50
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
+
+        # settings for the aliens
+        self.alien_speed_factor = 10
+        self.fleet_drop_speed = 10
+        # move direction of fleet, 1 right, -1 left
+        self.fleet_direction = 1 
 
