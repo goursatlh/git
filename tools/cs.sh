63765bc60d70ad19579a1b57761b26d8633212fe
goursatlh <goursatlh@163.com>
2024-08-30 16:46:23 +0800

    gtags file sync

--- tools/cs.sh
+++ tools/cs.sh
@@ -7,6 +7,11 @@ rm -f GPATH GRTAGS GTAGS
 #ctags -R &
 
 # use gtags instead of ctags/cscope to do the code index/reference
-find . -follow -name "*.c" -or -name "*.mib" -or -name "*.js" -or -name "*.php" -or -name "*.h" -or -name "*Makefile*" >gtags.files
+#find . -follow -name "*.c" -or -name "*.mib" -or -name "*.js" -or -name "*.php" -or -name "*.h" -or -name "*Makefile*" >gtags.files
+
+# exclude the directory .pc and soruce-pc , use -path xx -prune, not use -not -path, use -L, not use -follow
+find -L . \( -path "*/.pc/*" -o -path "*/source-pc/*" \) -prune -o \( -name "*.c" -o -name "*.h" \) 2>/dev/null -print >gtags. 
+files
+
 gtags 
 ~        
